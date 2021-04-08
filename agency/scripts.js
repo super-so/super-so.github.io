03a3cf6f50515cc584c19fa258c44a3ec67c93c2
traf <trafj@me.com>
2021-04-08 00:59:25 -0400

    Agency site updates

--- agency/scripts.js
+++ agency/scripts.js
@@ -1,6 +1,6 @@
 // Active nav links
-for (var i = 0; i < document.links.length; i++) {
-    if (document.links[i].href == document.URL) {
-        document.links[i].className = 'nav-active';
-    }
-}
+// for (var i = 0; i < document.links.length; i++) {
+//     if (document.links[i].href == document.URL) {
+//         document.links[i].className = 'nav-active';
+//     }
+// }
