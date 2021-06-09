435cf15b0fb539846dbd65d7657b40c8ab407a06
traf <trafj@me.com>
2021-06-08 23:08:30 -0400

    Super 2.0 site

--- super/scripts.js
+++ super/scripts.js
@@ -6,7 +6,9 @@ $(window).scroll(function(event){
      var scroll = $(this).scrollTop();
      if (scroll > 0){
          $('.notion-navbar').addClass('nav-scrolled');
+         $('.notion-quote').addClass('quote-scrolled');
      } else {
          $('.notion-navbar').removeClass('nav-scrolled');
+         $('.notion-quote').removeClass('quote-scrolled');
      }
 });
