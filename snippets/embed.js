f28d0be7fa99f506c00188a2ad7f0214db9274c6
traf <trafj@me.com>
2021-11-10 01:31:07 -0500

    Snippet code

---
+++ snippets/embed.js
@@ -0,0 +1,36 @@
+function clearBlock(el) {
+    const node = el.parentElement.parentElement;
+    node.innerHTML = '';
+    return node;
+}
+
+const SELECTOR = 'code:not([super-embed-seen])';
+function setupEmbeds() {
+
+    document.querySelectorAll(SELECTOR).forEach((node) => {
+        node.setAttribute('super-embed-seen', 1);
+        if (node.innerText.startsWith('super-embed:')) {
+            const code = node.innerText.replace('super-embed:', '');
+            const parentNode = clearBlock(node);
+            parentNode.innerHTML = code;
+            parentNode.querySelectorAll('script').forEach((script) => {
+                if (!script.src && script.innerText) {
+                    eval(script.innerText);
+                } else {
+                    const scr = document.createElement('script');
+                    scr.src = script.src;
+                    document.body.appendChild(scr);
+                }
+            })
+        }
+    })
+}
+
+setupEmbeds();
+
+var observer = new MutationObserver(function (mutations) {
+    if (document.querySelector(SELECTOR)) {
+        setupEmbeds();
+    }
+});
+observer.observe(document, { attributes: false, childList: true, characterData: false, subtree: true });
