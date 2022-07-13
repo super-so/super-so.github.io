106a55003f78d8b985725635bd85bb1838c7fb6f
Josh Millgate <hello@joshmillgate.co.uk>
2022-07-13 10:12:10 +0100

    update video script

--- superv3/js/script.js
+++ superv3/js/script.js
@@ -68,14 +68,17 @@ const onRouteChangeComplete = (url) => {
 
 const onRouteChangeStart = () => {
   if (next.router.state.route === '/') {
+    const video = document.getElementById("#block-579642c3c43749958ba0f1a38e9ecdb7 video");
     const openModalBtn = document.querySelector('#block-ca319f58fb8f4a0ab5bb814aeffd6062')
     const videoModal = document.querySelector('#block-0566e957626d48dfb97500a48d34fe8e')
     const closeModalBtn = document.querySelector('#block-89f4c511b4d945a586e530a8a1f4bc13')
     const onOpenModalBtnClick = () => {
       videoModal.classList.add("modal-open");
+      video.play();
     }
     const onCloseModalBtnClick = () => {
       videoModal.classList.remove("modal-open");
+      video.pause();
     }
 
     openModalBtn.removeEventListener('click', onOpenModalBtnClick);
