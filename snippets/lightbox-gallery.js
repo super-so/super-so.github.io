f8dfd10ca14116e6eebcbdf7f5e4c73ff100a83b
Josh Millgate <hello@joshmillgate.co.uk>
2023-04-16 10:58:25 +0100

    gallrey

---
+++ snippets/lightbox-gallery.js
@@ -0,0 +1,40 @@
+const initLightbox = () => {
+    const cards = document.querySelectorAll('.notion-collection-card');
+    const lightboxImage = document.querySelector('.lightbox-image');
+    const lightboxWrapper = document.querySelector('.lightbox-wrapper');
+    const closeLightbox = document.querySelector('.close-lightbox');
+
+    cards.forEach(card => {
+        card.addEventListener('click', (e) => {
+            lightboxImage.setAttribute("src", e.target.src);
+            lightboxWrapper.style.display = 'flex';
+            lightboxWrapper.classList.add('open');
+
+            console.log(e.target.src)
+
+        });
+    });
+
+    [lightboxWrapper, closeLightbox].forEach(button => {
+        button.addEventListener('click', (e) => {
+            if (lightboxWrapper.classList.contains = 'open') {
+                lightboxWrapper.style.display = 'none';
+                lightboxWrapper.classList.remove('open');
+                lightboxImage.setAttribute("src", '');
+            }
+        });
+    });
+
+    document.addEventListener('keydown', event => {
+        if (event.key === 'Escape') {
+            lightboxWrapper.style.display = 'none';
+        }
+    });
+}
+
+window.addEventListener('load', e => {
+    initLightbox();
+    next.router.events.on('routeChangeComplete', url => {
+        initLightbox();
+    })
+})
