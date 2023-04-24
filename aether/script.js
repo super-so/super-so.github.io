971da6baf9d66e17b95f94ca55492c995ce1ee58
Josh Millgate <hello@joshmillgate.co.uk>
2023-04-24 15:21:42 +0100

    agai

--- aether/script.js
+++ aether/script.js
@@ -17,7 +17,7 @@
 //             () => {
 //                 btn.innerHTML = `Copied! <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zm2 0h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
 //                 btn.setAttribute('style', 'opacity: 1');
-                
+
 //             },
 //             () => alert('failed to copy'),
 //         );
@@ -35,9 +35,30 @@
 //     });
 // }
 
+
+const addCopyButtonToCodeBlocks = () => {
+    const codeBlocks = document.querySelectorAll('code[class*="language-"]');
+
+    codeBlocks.forEach((codeBlock) => {
+        const button = document.createElement('button');
+        button.classList.add('copy-to-clipboard-button')
+        button.innerHTML = `Copy <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
+
+        button.addEventListener('click', () => {
+            navigator.clipboard.writeText(codeBlock.innerText);
+            button.innerHTML = `Copied! <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zm2 0h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
+            setTimeout(() => {
+                button.innerHTML = `Copy <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
+            }, 2000);
+        });
+        codeBlock.parentNode.insertBefore(button, codeBlock.nextSibling);
+    });
+}
+
+
 const setActivePage = () => {
     const currentPage = document.querySelectorAll('a[href="' + window.location.pathname + '"]');
-    currentPage.forEach(function(page) {
+    currentPage.forEach(function (page) {
         if (!page.classList.contains('super-navbar__logo') && !page.classList.contains('super-footer__logo') && !page.parentNode.classList.contains('notion-image')) {
             page.classList.add('page-active')
         }
@@ -50,10 +71,10 @@ const setActivePage = () => {
 
 window.addEventListener('load', e => {
     setActivePage()
-    // initCodeCopy()
+    addCopyButtonToCodeBlocks()
 
     next.router.events.on('routeChangeComplete', url => {
         setActivePage()
-        // initCodeCopy()
+        addCopyButtonToCodeBlocks()
     })
 })
