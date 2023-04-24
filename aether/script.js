6bcb3a02a9e2612d6d0fee1ef0f76dbdf07c49f4
Josh Millgate <hello@joshmillgate.co.uk>
2023-04-24 14:29:49 +0100

    fix

--- aether/script.js
+++ aether/script.js
@@ -2,7 +2,6 @@ function initCodeCopy() {
     const codeBlocks = document.querySelectorAll('code[class*="language-"]');
 
     codeBlocks.forEach((block) => {
-        const lang = parseLanguage(block);
         const referenceEl = block.parentElement;
         const parent = block.parentElement.parentElement;
         
@@ -14,18 +13,10 @@ function initCodeCopy() {
         const copyBtn = document.createElement('button');
         copyBtn.setAttribute('class', 'copy-button');
         copyBtn.setAttribute('data-lang', lang);
-        copyBtn.innerHTML = `${lang} <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
+        copyBtn.innerHTML = `Copy <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
 
         wrapper.insertAdjacentElement('beforeend', copyBtn);
     });
-
-    function parseLanguage(block) {
-        const className = block.className;
-        if (className.startsWith('language')) {
-            const [prefix, lang] = className.split('-');
-            return lang;
-        }
-    }
 
     function copy(e) {
         const btn = e.currentTarget;
@@ -35,7 +26,7 @@ function initCodeCopy() {
 
         navigator.clipboard.writeText(text).then(
             () => {
-                btn.innerHTML = `copied! <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zm2 0h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
+                btn.innerHTML = `Copied! <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zm2 0h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
                 btn.setAttribute('style', 'opacity: 1');
                 
             },
@@ -44,7 +35,7 @@ function initCodeCopy() {
 
         setTimeout(() => {
             btn.removeAttribute('style');
-            btn.innerHTML = `${lang} <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
+            btn.innerHTML = `Copy <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
         }, 3000);
     }
 
