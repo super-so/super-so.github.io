377863eebae68972f5c58a439de3da63353fa1c7
Josh Millgate <hello@joshmillgate.co.uk>
2023-04-24 14:57:58 +0100

    typo

--- aether/script.js
+++ aether/script.js
@@ -1,21 +1,12 @@
 const initCodeCopy = () => {
-    const codeBlocks = document.querySelectorAll('code[class*="language-"]');
+    const codeBlocks = document.querySelectorAll('pre[class*="language-"]');
 
     codeBlocks.forEach((block) => {
-        const referenceEl = block.parentElement;
-        const parent = block.parentElement.parentElement;
-        
-        const wrapper = document.createElement('div');
-        wrapper.className = 'code-wrapper';
-        parent.insertBefore(wrapper, referenceEl);
-        wrapper.append(block.parentElement);
-
         const copyBtn = document.createElement('button');
         copyBtn.setAttribute('class', 'copy-button');
-        copyBtn.setAttribute('data-lang', lang);
         copyBtn.innerHTML = `Copy <svg viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M7 6V3a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1h-3v3c0 .552-.45 1-1.007 1H4.007A1.001 1.001 0 0 1 3 21l.003-14c0-.552.45-1 1.007-1H7zM5.003 8L5 20h10V8H5.003zM9 6h8v10h2V4H9v2z" fill="currentColor"/></svg>`;
 
-        wrapper.insertAdjacentElement('beforeend', copyBtn);
+        block.appendChild(copyBtn);
     });
 
     function copy(e) {
