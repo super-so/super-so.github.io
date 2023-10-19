a71e6561b262baccc737432d8ff9af6c01a60bc8
Josh Millgate <hello@joshmillgate.co.uk>
2023-10-19 10:32:34 +0100

    update lightbox

--- snippets/lightbox.js
+++ snippets/lightbox.js
@@ -1,20 +1,26 @@
-const initLightbox = () => {
+const attachListenersToImages = () => {
     const images = document.querySelectorAll('.notion-image');
-    const lightboxImage = document.querySelector('.lightbox-image');
-    const lightboxWrapper = document.querySelector('.lightbox-wrapper');
-    const closeLightbox = document.querySelector('.close-lightbox');
-
     images.forEach(image => {
         image.addEventListener('click', (e) => {
+            const lightboxImage = document.querySelector('.lightbox-image');
+            const lightboxWrapper = document.querySelector('.lightbox-wrapper');
             lightboxImage.setAttribute("src", e.target.src);
             lightboxWrapper.style.display = 'flex';
             lightboxWrapper.classList.add('open');
         });
     });
+};
+
+const initLightbox = () => {
+    const lightboxImage = document.querySelector('.lightbox-image');
+    const lightboxWrapper = document.querySelector('.lightbox-wrapper');
+    const closeLightbox = document.querySelector('.close-lightbox');
+
+    attachListenersToImages();
 
     [lightboxWrapper, closeLightbox].forEach(button => {
         button.addEventListener('click', (e) => {
-            if (lightboxWrapper.classList.contains = 'open') {
+            if (lightboxWrapper.classList.contains('open')) {
                 lightboxWrapper.style.display = 'none';
                 lightboxWrapper.classList.remove('open');
                 lightboxImage.setAttribute("src", '');
@@ -27,11 +33,36 @@ const initLightbox = () => {
             lightboxWrapper.style.display = 'none';
         }
     });
-}
+};
+
+const observeNewImages = () => {
+    const observerConfig = { childList: true, subtree: true, attributes: true, attributeFilter: ['src'] };
+    const observer = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) {
+            if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                for (const node of mutation.addedNodes) {
+                    if (node.matches && node.matches('.notion-image')) {
+                        attachListenersToImages();
+                        break;
+                    }
+                }
+            } else if (mutation.type === 'attributes' && mutation.target.matches('.notion-image')) {
+                attachListenersToImages();
+            }
+        }
+    });
+
+    observer.observe(document.body, observerConfig);
+};
 
 window.addEventListener('load', e => {
-    initLightbox();
-    next.router.events.on('routeChangeComplete', url => {
+    setTimeout(function () {
+
         initLightbox();
-    })
-})
+        observeNewImages();
+        next.router.events.on('routeChangeComplete', url => {
+            initLightbox();
+        });
+    }, 500);  // 2000ms = 2 seconds delay
+
+});
