b1f0d837367e3c9022fc08c64cb56d9124fcdaa1
Jason Werner <jason@trxrg.com>
2024-10-06 15:12:33 -0600

    comment out aether script contents

--- aether/script.js
+++ aether/script.js
@@ -1,20 +1,20 @@
-const setActivePage = () => {
-    const currentPage = document.querySelectorAll('a[href="' + window.location.pathname + '"]');
-    currentPage.forEach(function (page) {
-        if (!page.classList.contains('super-navbar__logo') && !page.classList.contains('super-footer__logo') && !page.parentNode.classList.contains('notion-image')) {
-            page.classList.add('page-active')
-        }
-        const pageIcon = page.querySelector('.notion-page__icon')
-        if (pageIcon) {
-            pageIcon.setAttribute("style", "opacity:1!important; filter:grayscale(0%)!important;")
-        }
-    });
-}
+// const setActivePage = () => {
+//     const currentPage = document.querySelectorAll('a[href="' + window.location.pathname + '"]');
+//     currentPage.forEach(function (page) {
+//         if (!page.classList.contains('super-navbar__logo') && !page.classList.contains('super-footer__logo') && !page.parentNode.classList.contains('notion-image')) {
+//             page.classList.add('page-active')
+//         }
+//         const pageIcon = page.querySelector('.notion-page__icon')
+//         if (pageIcon) {
+//             pageIcon.setAttribute("style", "opacity:1!important; filter:grayscale(0%)!important;")
+//         }
+//     });
+// }
 
-window.addEventListener('load', e => {
-    setActivePage()
+// window.addEventListener('load', e => {
+//     setActivePage()
 
-    window.events.on('routeChangeComplete', url => {
-        setActivePage()
-    })
-})
+//     window.events.on('routeChangeComplete', url => {
+//         setActivePage()
+//     })
+// })
